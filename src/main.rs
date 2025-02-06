5479c523dfa2826d5ffe568e9098b28495a2d6c8
topongo <lorenzo.bodini.private@gmail.com>
2025-02-06 16:12:20 +0100

    fix notifications, add beep feature

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
 use std::process::exit;
-use chrono::Duration;
+use chrono::{Duration, TimeDelta};
 
 use chrono::{Local, NaiveTime};
-use clap::{Parser, Subcommand};
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 struct Command {
-    #[command(subcommand)]
-    mode: Mode,
     #[cfg(feature = "notify")]
     #[arg(short, long)]
     title: Option<String>,
+    mode: Mode,
+    reference: String,
     #[arg(trailing_var_arg = true)]
-    reference: Vec<String>,
+    exec: Vec<String>,
 }
 
-#[derive(Debug, Subcommand, Clone)]
+#[derive(Debug, ValueEnum, Clone)]
 enum Mode {
-    At {
-        reference: String,
-    },
-    In {
-        #[arg(trailing_var_arg = true)]
-        trailing: Vec<String>,
-    }
+    At,
+    In,
 }
 
 fn format_timedelta(delta: Duration) -> String {
@@ -60,48 +55,72 @@ static CHECKPOINTS: [u64; 4] = [
     10,
 ];
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Command::parse();
 
-    let mut timeout = match args.mode {
-        Mode::At { reference } => {
+    let timeout = match args.mode {
+        Mode::At => {
             // colon separated time
-            match reference.chars().filter(|c| *c == ':').count() {
+            let target = match args.reference.chars().filter(|c| *c == ':').count() {
                 0 => {
-                    // check for plain seconds
-                    match reference.parse::<u64>() {
-                        Ok(v) => Duration::seconds(v as i64),
+                    // check for hh
+                    match args.reference.parse::<u32>() {
+                        Ok(v) => match NaiveTime::from_hms_opt(v, 0, 0) {
+                            Some(t) => t - Local::now().time(),
+                            None => {
+                                eprintln!("couldn't parse reference time: {}", args.reference);
+                                exit(1);
+                            }
+                        }
                         Err(_) => {
-                            // try using h m s shorthands
-                            eprintln!("couldn't parse reference time");
+                            eprintln!("couldn't parse reference time: {}", args.reference);
                             exit(1);
                         }
                     }
                 }
-                1 => {
-                    // check fo hh:mm
-                    let parsed: NaiveTime = reference.parse().unwrap();
-                    let mut target = parsed - Local::now().time();
-                    if target < Duration::seconds(0) {
-                        target += Duration::days(1);
+                1 | 2 => {
+                    // check for hh:mm:ss
+                    match args.reference.parse::<NaiveTime>() {
+                        Ok(v) => v - Local::now().time(),
+                        Err(_) => {
+                            eprintln!("couldn't parse reference time: {}", args.reference);
+                            exit(1);
+                        }
                     }
-                    target
                 }
                 _ => todo!()
+            };
+            if target < Duration::zero() {
+                target + Duration::days(1)
+            } else {
+                target
             }
         }
-        Mode::In { trailing } => {
-            let trailing = trailing.join(" ");
-            let caps = regex::Regex::new(r"((\d+)h)? ?((\d+)m)? ?((\d+)s)?").unwrap().captures(&trailing).unwrap();
-            println!("{:?}", caps.iter().collect::<Vec<_>>());
+        Mode::In => {
+            // safe unwrap: regex is ok at compile time
+            let caps = match regex::Regex::new(r"((\d+)h)? ?((\d+)m)? ?((\d+)s)?").unwrap().captures(&args.reference) {
+                Some(c) => c,
+                None => {
+                    eprintln!("couldn't parse reference time");
+                    exit(1);
+                }
+            };
+            // println!("{:?}", caps.iter().collect::<Vec<_>>());
             caps.iter()
                 .skip(2)
                 .enumerate()
                 .filter(|(i, _)| i % 2 == 0)
                 .filter_map(|(i, m)| m.map(|v| (i, v)))
                 .fold(Duration::zero(), |acc, (i, m)| {
-                    let value = m.as_str().parse::<i64>().unwrap();
-                    println!("{}: {}", i, value);
+                    let value = match m.as_str().parse::<i64>() {
+                        Ok(v) => v,
+                        Err(_) => {
+                            eprintln!("invalid numeric value: {}", m.as_str());
+                            exit(1);
+                        }
+                    };
+                    // println!("{}: {}", i, value);
                     acc + match i {
                         0 => Duration::hours(value),
                         2 => Duration::minutes(value),
@@ -111,27 +130,83 @@ fn main() {
                 })
         }
     };
+
+    // make program exit if command is invalid
+    let command = match args.exec.len() {
+        0 => None,
+        _ => {
+            let mut cmd = std::process::Command::new(&args.exec[0]);
+            cmd.args(&args.exec[1..]);
+            Some(cmd)
+        }
+    };
+
     println!("Timer will go off in {} (at {})", format_timedelta(timeout), Local::now() + timeout);
 
     let title = args.title.unwrap_or("Unnamed timer".to_owned());
+
+    let mut notifiers = vec![];
     for checkpoint in CHECKPOINTS.iter().chain(vec![&0]).map(|&v| Duration::seconds(v as i64)) {
         // skip checkpoint lower than timeout
         if timeout <= checkpoint {
             continue;
         }
-        #[cfg(debug_assertions)]
-        println!("==> sleeping for {:?}", timeout - checkpoint);
-        #[cfg(not(debug_assertions))]
-        std::thread::sleep((timeout - checkpoint).to_std().unwrap());
-        timeout = checkpoint;
-        #[cfg(feature = "beep")]
-        print!("\x07");
-        #[cfg(feature = "notify")]
-        notify_rust::Notification::new()
-            .summary(&format!("{}", title))
-            .body(&format!("{} will end in {}", title, format_timedelta(timeout)))
-            .show().unwrap();
-
+        // start async waiters
+        notifiers.push(tokio::spawn(wait_and_notify(timeout, checkpoint, title.clone())));
     }
+
+    // wait for all async waiters to finish
+    for n in notifiers {
+        n.await.unwrap();
+    }
+
+    // execute end command if provided, otherwise 
+    match command {
+        Some(mut cmd) => {
+            let status = cmd
+                .spawn()
+                .unwrap()
+                .wait()
+                .unwrap();
+            if !status.success() {
+                eprintln!("command failed: {:?}", cmd);
+                exit(1);
+            }
+        }
+        None => notify(&title, "Time's up!"),
+    }
+    // if !args.exec.is_empty() {
+    //     let mut cmd = std::process::Command::new(&args.exec[0])
+    //         .args(&args.exec[1..])
+    //         .spawn().unwrap();
+    //     cmd.wait().unwrap();
+    // } else {
+    //     notify(&title, "Time's up!");
+    // }
 }
 
+async fn wait_and_notify(timeout: TimeDelta, checkpoint: TimeDelta, title: String) {
+    let wait = timeout - checkpoint;
+    #[cfg(debug_assertions)]
+    println!("==> sleeping for {:?}", wait);
+    #[cfg(not(debug_assertions))]
+    {
+        use tokio::time::Instant;
+        let target = Instant::now() + wait.to_std().expect("timeout is negative");
+        // println!("==> sleeping until {:?}", target);
+        tokio::time::sleep_until(target).await;
+    }
+    notify(&title, &format!("Will end in {}", format_timedelta(checkpoint)));
+}
+
+fn notify(title: &str, body: &str) {
+    #[cfg(feature = "notify")]
+    notify_rust::Notification::new()
+        .summary(title)
+        .body(body)
+        .show().unwrap();
+    #[cfg(not(feature = "notify"))]
+    println!("{}: {}", title, body);
+    #[cfg(feature = "beep")]
+    print!("\x07");
+}
